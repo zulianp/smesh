494fffd1a7406b94f886c00379d571d67ec729e5
Patrick Zulian <patrick.zulian@gmail.com>
2026-02-07 20:39:11 +0100

    ...

--- src/mesh/semistructured/smesh_sshex8_mesh.cpp
+++ src/mesh/semistructured/smesh_sshex8_mesh.cpp
@@ -3,21 +3,21 @@
 #include "smesh_sshex8_mesh.impl.hpp"
 #include "smesh_types.hpp"
 
-#define SMESH_EXPLICIT_INSTANTIATE_SSHEX8_MESH(IDX_T, GEOM_T)                   \
-  template int sshex8_to_standard_hex8_mesh<IDX_T>(                             \
-      const int, const ptrdiff_t,                                               \
-      IDX_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                        \
-      IDX_T *SMESH_RESTRICT *const SMESH_RESTRICT);                             \
-  template int sshex8_fill_points<IDX_T, GEOM_T>(                               \
-      const int, const ptrdiff_t,                                               \
-      IDX_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                        \
-      GEOM_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                       \
-      GEOM_T *SMESH_RESTRICT *const SMESH_RESTRICT);                            \
-  template int sshex8_fill_points_1D_map<IDX_T, GEOM_T>(                         \
-      const int, const ptrdiff_t,                                               \
-      IDX_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                        \
-      GEOM_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                       \
-      const GEOM_T *const SMESH_RESTRICT,                                       \
+#define SMESH_EXPLICIT_INSTANTIATE_SSHEX8_MESH(IDX_T, GEOM_T)                  \
+  template int sshex8_to_standard_hex8_mesh<IDX_T>(                            \
+      const int, const ptrdiff_t,                                              \
+      IDX_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                       \
+      IDX_T *SMESH_RESTRICT *const SMESH_RESTRICT);                            \
+  template int sshex8_fill_points<IDX_T, GEOM_T>(                              \
+      const int, const ptrdiff_t,                                              \
+      IDX_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                       \
+      GEOM_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                      \
+      GEOM_T *SMESH_RESTRICT *const SMESH_RESTRICT);                           \
+  template int sshex8_fill_points_1D_map<IDX_T, GEOM_T>(                       \
+      const int, const ptrdiff_t,                                              \
+      IDX_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                       \
+      GEOM_T *const SMESH_RESTRICT *const SMESH_RESTRICT,                      \
+      const GEOM_T *const SMESH_RESTRICT,                                      \
       GEOM_T *SMESH_RESTRICT *const SMESH_RESTRICT)
 
 namespace smesh {
