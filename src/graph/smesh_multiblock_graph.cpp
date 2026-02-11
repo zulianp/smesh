95b9b98cb5dcf2ede823bb697e231fb848fb92dd
Patrick Zulian <patrick.zulian@gmail.com>
2026-02-11 16:43:40 +0100

    ...

--- src/graph/smesh_multiblock_graph.cpp
+++ src/graph/smesh_multiblock_graph.cpp
@@ -8,23 +8,26 @@
 #define SMESH_EXPLICIT_INSTANTIATE_MULTIBLOCK_GRAPH(T)                         \
   template int create_multiblock_n2e<T, T, T>(                                 \
       block_idx_t, const enum ElemType *, const ptrdiff_t *,                   \
-      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, ptrdiff_t, block_idx_t **, T **, T **);      \
+      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, ptrdiff_t,        \
+      block_idx_t **, T **, T **);                                             \
   template int create_multiblock_crs_graph_from_n2e<T, T, T>(                  \
       block_idx_t, const enum ElemType *, const ptrdiff_t *, ptrdiff_t,        \
-      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, const T *const SMESH_RESTRICT,               \
-      const T *const SMESH_RESTRICT, const block_idx_t *const SMESH_RESTRICT,  \
-      T **, T **);                                                             \
+      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *,                   \
+      const T *const SMESH_RESTRICT, const T *const SMESH_RESTRICT,            \
+      const block_idx_t *const SMESH_RESTRICT, T **, T **);                    \
   template int create_multiblock_crs_graph<T, T, T>(                           \
       block_idx_t, const enum ElemType *, const ptrdiff_t *,                   \
-      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, ptrdiff_t, T **, T **);                      \
+      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, ptrdiff_t, T **,  \
+      T **);                                                                   \
   template int create_multiblock_crs_graph_upper_triangular_from_n2e<T, T, T>( \
       block_idx_t, const enum ElemType *, const ptrdiff_t *, ptrdiff_t,        \
-      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, const T *const SMESH_RESTRICT,               \
-      const T *const SMESH_RESTRICT, const block_idx_t *const SMESH_RESTRICT,  \
-      T **, T **);                                                             \
+      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *,                   \
+      const T *const SMESH_RESTRICT, const T *const SMESH_RESTRICT,            \
+      const block_idx_t *const SMESH_RESTRICT, T **, T **);                    \
   template int create_multiblock_crs_graph_upper_triangular<T, T, T>(          \
       block_idx_t, const enum ElemType *, const ptrdiff_t *,                   \
-      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, ptrdiff_t, T **, T **)
+      const T *const SMESH_RESTRICT *const SMESH_RESTRICT *, ptrdiff_t, T **,  \
+      T **)
 
 namespace smesh {
 SMESH_EXPLICIT_INSTANTIATE_MULTIBLOCK_GRAPH(i32);
